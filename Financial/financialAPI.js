3648de0822ada7740a24d6dd3ce2eaadab9b5a35
jungsh404 <jung-sh@naver.com>
2025-02-04 14:55:05 +0900

    referer 서버 죽는 문제 해결

--- Financial/financialAPI.js
+++ Financial/financialAPI.js
@@ -44,9 +44,9 @@ app.use((req, res, next) => {
 // GET 요청 처리
 app.get('/api/financials', async (req, res) => {
     const { report_period, financial_name, company_name, ranking, order_by, limit } = req.query;
-
+    let query = `SELECT report_period, financial_name, company_name, data, ranking, difer_data FROM financial_rank_table WHERE 1=1`;
     try {
-        let query = `SELECT report_period, financial_name, company_name, data, ranking, difer_data FROM financial_rank_table WHERE 1=1`;
+        
         let values = [];
 
         // ✅ 다중 값 OR 처리 (쉼표 `,`로 구분된 값)
